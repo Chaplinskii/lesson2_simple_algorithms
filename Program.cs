20b7bf278b6e772f4b8508acf975b8d9c361483b
Chaplinskii <6945053@gmail.com>
2024-01-29 22:53:27 +0300

    решил задачу 2

--- Program.cs
+++ Program.cs
@@ -4,16 +4,17 @@
 // a=50 => нет
 // a=7 => нет
 // a=322 => да
-Console.Clear();
-Console.Write("Введите число: ");
-int n = int.Parse(Console.ReadLine()!);
-n = Math.Abs(n);
-if (n%7==0 || n%23==0){
-    Console.WriteLine($"Число {n} кратно 7 и 23");
-}
-else{
-    Console.WriteLine($"Число {n} не кратно 7 и 23");
-}
+
+// Console.Clear();
+// Console.Write("Введите число: ");
+// int n = int.Parse(Console.ReadLine()!);
+// n = Math.Abs(n);
+// if (n%7==0 || n%23==0){
+//     Console.WriteLine($"Число {n} кратно 7 и 23");
+// }
+// else{
+//     Console.WriteLine($"Число {n} не кратно 7 и 23");
+// }
 
 // Задача 2: Напишите программу, которая принимает
 // на вход координаты точки (X и Y), причём X ≠ 0 и Y ≠
@@ -23,6 +24,36 @@ else{
 // -5, 3 => 2
 // -3, -2 => 3
 // 4, -2 => 4
+
+using System.IO.Compression;
+
+Console.Clear();
+Console.Write("Введите координату X: ");
+int x = int.Parse(Console.ReadLine()!);
+while (x==0){
+    Console.Write("Ошибка! X не должен быть равен 0! \nВведите координату X: ");
+    x = int.Parse(Console.ReadLine()!);
+}
+Console.Write("Введите координату Y: ");
+int y = int.Parse(Console.ReadLine()!);
+int z=0;
+while (y==0){
+    Console.Write("Ошибка! Y не долженбыть равен 0! \nВведите координату Y: ");
+    y = int.Parse(Console.ReadLine()!);
+}
+if (x>0 && y>0){
+    z=1;
+}
+else if (x<0 && y>0){
+    z=2;
+}
+else if (x<0 && y<0){
+    z=3;
+}
+else if (x>0 && y<0){
+    z=4;
+}
+Console.WriteLine($"При координатах точки X={x} и Y={y} она относится к {z}-ой координатной четверти плоскости!");
 
 // Задача 3: Напишите программу, которая принимает
 // на вход целое число из отрезка [10, 99] и показывает
